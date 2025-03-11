656510e9d06735f0795d54d3ed1f09ad6c0eff2e
PC-F-Dosen <arthawebid@gmail.com>
2025-03-11 21:55:04 +0800

    Pertemuan 2

---
+++ pertemuan2/src/pertemuan2/Pertemuan2.java
@@ -0,0 +1,24 @@
+package pertemuan2;
+
+/**
+ *
+ * @author Made Artha
+ * tgl: 11 Maret 2025
+ */
+public class Pertemuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Nama Anda";
+        String nim = "1234567890";
+        
+        System.out.println("Pertemuan 2 Java");
+        System.out.println("~~~~~~~~~~~~~~~~");
+        System.out.println("Nama   : "+nama);
+        System.out.println("NIM    : "+nim);
+        System.out.printf("Nama saya %S dengan NIM %s, saat ini sedang belajar pemrograman bahasa Java",nama,nim );
+    }
+    
+}
